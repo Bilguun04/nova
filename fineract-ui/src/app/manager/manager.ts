09128aa6e553995d16003bf9b99b6b060ba2404e
Bilguun04 <bilguunbek0607@gmail.com>
2026-05-21 12:50:30 -0400

    feat: add admin, auth, manager, middleware, and public components with basic structure and tests

---
+++ fineract-ui/src/app/manager/manager.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager',
+  imports: [],
+  templateUrl: './manager.html',
+  styleUrl: './manager.css',
+})
+export class Manager {}
---
+++ fineract-ui/src/app/middleware/middleware.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-middleware',
+  imports: [],
+  templateUrl: './middleware.html',
+  styleUrl: './middleware.css',
+})
+export class Middleware {}
